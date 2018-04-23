43326432b057f340101f615152a53165348710b0
Daniel H <dnlheelan@gmail.com>
2018-04-23 19:30:02 +0100

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,21 +1,26 @@
 import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.*;
 
 public class test{
+	
 	public static void main(String args[]){
+		
+		float PI = 3.14159265359f;
 		
 		String sideSquareAsString = JOptionPane.showInputDialog("Please enter the side of the square");
 		float resultSideSquare = Float.parseFloat(sideSquareAsString);
-		JOptionPane.showMessageDialog(null, "The area of the square is " + areaOfSquare(result) + ".", "", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "The area of the square is " + areaOfSquare(resultSideSquare) + ".", "", JOptionPane.INFORMATION_MESSAGE);
 		
-		String radiusCircleAsString = JOptionPane.showInputDialogDialog("Please enter the radius of the circle");
-		float resultRadiusCircleAsString = Float.parseFloat(radiusCircleAsString);
-		JOptionPane.showMessageDialog(null, "The area of the circle is: " + areaOfCircle(resultAreaCircleAsString), "Area of Circle", JOptionPane.INFORMATION_MESSAGE);
+		String radiusCircleAsString = JOptionPane.showInputDialog("Please enter the radius of the circle");
+		float resultRadiusCircle = Float.parseFloat(radiusCircleAsString);
+		JOptionPane.showMessageDialog(null, "The area of the circle is: " + areaOfCircle(resultRadiusCircle), "Area of Circle", JOptionPane.INFORMATION_MESSAGE);
 		
-		String widthRectangleAsString = JOptionPane.showInputDialogDialog("Please enter the width of the rectangle");
-		String lengthRectangleAsString = JOptionPane.showInputDialog("Please enter the length of the rectangle")
-		float resultWidthRectangleAsString = Float.parseFloat(widthRectangleAsString);
-		float resultLengthRectangleAsString = Float.parseFloat(lengthRectangleAsStringAsString);
-		JOptionPane.showMessageDialog(null, "The area of the rectangle is " + areaOfRectangle(widthRectangleAsString, lengthRectangleAsString), "Area of Rectangle", JOptionPane.INFORMATION_MESSAGE);
+		String widthRectangleAsString = JOptionPane.showInputDialog("Please enter the width of the rectangle");
+		String lengthRectangleAsString = JOptionPane.showInputDialog("Please enter the length of the rectangle");
+		float resultWidthRectangle = Float.parseFloat(widthRectangleAsString);
+		float resultLengthRectangle = Float.parseFloat(lengthRectangleAsString);
+		JOptionPane.showMessageDialog(null, "The area of the rectangle is " + areaOfRectangle(resultWidthRectangle, resultLengthRectangle), "Area of Rectangle", JOptionPane.INFORMATION_MESSAGE);
 		
 
 	}
@@ -28,70 +33,69 @@ public class test{
 	}
 	
 	public static float areaOfCircle (float radius){
-		float result = String.format("%.2f",(PI * Math.pow(radius, 2)));
+		float result = 3.14 * radius * radius
 
-		
 		return result;
 	}
 	
 	public static float areaOfRectangle (float length, float width){
-		float result = width * length
+		float result = width * length;
 
 		return result;
 	}
 	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
+	public static float perimeterOfSquare (float side){
+		float result = 4*side;
 
-		return maximum;
+		return result;
 	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-	
-	public static float max (float x, float y){
-		float maximum = Math.max(x, y);
-
-		return maximum;
-	}
-
 }
+	
+	/*public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+*/
