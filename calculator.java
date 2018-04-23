90868150b5be87abde9ab9a998b06f0577ae3752
Daniel H <dnlheelan@gmail.com>
2018-04-23 19:07:29 +0100

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+
+public class test{
+	public static void main(String args[]){
+		
+		String sideSquareAsString = JOptionPane.showInputDialog("Please enter the side of the square");
+		float resultSideSquare = Float.parseFloat(sideSquareAsString);
+		JOptionPane.showMessageDialog(null, "The side of the square is " + areaOfSquare(result) + ".", "", JOptionPane.INFORMATION_MESSAGE);
+		
+		String areaCircleAsString = JOptionPane.showInputDialogDialog("Please enter the radius of the circle");
+		float areaCircleAsString = Float.parseFloat(areaCircleAsString);
+		JOptionPane.showMessageDialog(null, "The area of the circle is: " + areaOfCircle(resultAreaCircleAsString), "Area of Circle", JOptionPane.INFORMATION_MESSAGE);
+		
+
+	}
+
+	public static float areaOfSquare(float side){
+		float result = side*side;
+
+		return result;
+
+	}
+	
+	public static float areaOfCircle (float radius){
+		float result = String.format("%.2f",(PI * Math.pow(radius, 2)));
+
+		
+		return result;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+	
+	public static float max (float x, float y){
+		float maximum = Math.max(x, y);
+
+		return maximum;
+	}
+
+}
